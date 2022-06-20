2c778da2b387905162e61fdb915aaa1a55b839c1
Shashankrao06 <shashankaibaraje@gmail.com>
2022-06-20 20:44:18 +0530

    Added data project

---
+++ Database Backup/EmployeeManagement.Data/Models/CompanyModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagement.Data.Models
+{
+    public class CompanyModel
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyPhone { get; set; }
+
+    }
+}
---
+++ Database Backup/EmployeeManagement.Data/Models/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeManagement.Data.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public int ProjectId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string DateCreated { get; set; }
+        public string DateModified { get; set; }
+
+        public string? CompanyName { get; set; }
+        public string? CompanyPhone { get; set; }
+    }
+}
