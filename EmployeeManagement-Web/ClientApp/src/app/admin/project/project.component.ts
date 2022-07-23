646092fd49b78faf6681bb6232bfea4c18bafd35
Asha <ashalatha@aykansoft.com>
2022-07-23 19:37:27 +0530

    UI Changes

--- EmployeeManagement-Web/ClientApp/src/app/admin/project/project.component.ts
+++ EmployeeManagement-Web/ClientApp/src/app/admin/project/project.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from 'src/app/core/service/authentication.service';
+import { ProjectModel } from 'src/app/login/models/project.model';
 
 @Component({
   selector: 'app-project',
@@ -10,7 +11,9 @@ import { AuthenticationService } from 'src/app/core/service/authentication.servi
 export class ProjectComponent implements OnInit {
   ProjectForm!: FormGroup;
   allProjectDetails: any = [];
+  allProjDetails: ProjectModel[] = [];
   show: boolean=false;
+  showUpdate: boolean=false;
   Submit: boolean=true;
   EditCheck: boolean=false;
   projectid: any;
@@ -52,7 +55,7 @@ export class ProjectComponent implements OnInit {
         this.authenticationService.EditProject( projectId,projectname,projectdescription)
       .subscribe(
         (data : any) => {
-          this.show = true;
+          this.showUpdate = true;
         })
       }
        
@@ -62,13 +65,20 @@ export class ProjectComponent implements OnInit {
     this.authenticationService.getProjDetails()
     .subscribe(
       (data : any) => {
-        this.allProjectDetails = data;
+        this.allProjDetails = data;
       })
   }
   closePopup()
   {
     this.show = !this.show; 
-    this.clearForm;
+    this.clearForm();
+    this.getProjectDetails();
+  }
+
+  closeUpdatePopup(){
+    this.showUpdate = !this.showUpdate; 
+    this.Submit = true;
+    this.clearForm();
     this.getProjectDetails();
   }
   clearForm() {
@@ -96,9 +106,17 @@ export class ProjectComponent implements OnInit {
       })
   }
 
-
-
+  SearchDetails(val: any){
+    debugger;
+    if(val!='')
+    {
+      this.allProjDetails = this.allProjDetails.filter(x=>x.projectName==val || x.projectDesc==val); 
+    }else{
+      this.getProjectDetails();
+    }
   }
+
+}
 
 
 
